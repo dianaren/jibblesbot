d35eb14d5a3b0a01a78f8ea7b66e4da9401551f6
Diana Ren <dren@tenable.com>
2020-10-01 16:17:40 -0700

    add a list of things that may be bought

--- commands/bought.js
+++ commands/bought.js
@@ -1,11 +1,21 @@
+const stuff = {
+  'skis': 'have',
+  'gear': 'has'
+}
+
 module.exports = {
 	name: 'bought',
 	description: 'Just bought stuff!',
   args: 1,
   usage: '<stuff>',
 	execute(message, args) {
-      let daysSinceSkisBought = 0;
-      message.channel.setTopic(`${daysSinceSkisBought} day(s) since skis have been bought! Last one to buy skis: ${message.author.username}`)
-        .then(updated => message.channel.send(`SPENT IT! ${message.author.username} bought skis! The channel topic has been updated.`));
+    let stuffBought = args[0];
+    if (!stuff[stuffBought]) {
+      message.reply(`Congrats on your new ${stuffBought} but I'm not tracking that yet!`);
+    }
+    // TODO: retrieve a real value indexed by stuffBought
+    let fakeCounter = 0;
+    message.channel.setTopic(`${fakeCounter} day(s) since ${stuffBought} ${stuff[stuffBought]} been bought! Last one to buy skis: ${message.author.username}`)
+      .then(updated => message.channel.send(`SPENT IT! ${message.author.username} bought skis! The channel topic has been updated.`));
 	},
 };
