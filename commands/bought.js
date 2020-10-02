61f2275eac4e2fd28d5c2acf715031f99faadb2a
Diana Ren <44383284+dianaren@users.noreply.github.com>
2020-10-02 12:23:30 -0700

    Add statefulness to the bought command (#4)

--- commands/bought.js
+++ commands/bought.js
@@ -1,6 +1,42 @@
+const uuidv4 = require('uuid').v4;
+
 const buyableStuff = {
   'skis': 'have',
   'gear': 'has'
+}
+
+async function addNewPurchase(username, collection) {
+  const uuid = uuidv4();
+  const update = {
+    $set: {
+      uuid: uuid,
+      username: username,
+    },
+    $currentDate: {
+        purchaseTime: true,
+     }
+  };
+  await collection.updateOne({ uuid: uuid }, update, { upsert: true });
+}
+
+async function updateGlobalStats(username, collection) {
+  const query = { globalStats: { $exists: true } };
+  const update = {
+    $set: {
+      'globalStats.lastPurchasedBy': username,
+      'globalStats.daysSincePurchased': 0
+    },
+    $inc: {
+      'globalStats.totalPurchased': 1
+    }
+  };
+  await collection.updateOne(query, update, { upsert: true });
+}
+
+function updateMongoPurchases(stuffBought, username, mongo) {
+  const collection = mongo.db('default').collection(stuffBought);
+  // TODO: these can fail independently of each other. make them atomic
+  addNewPurchase(username, collection).then(updateGlobalStats(username, collection));
 }
 
 module.exports = {
@@ -8,14 +44,14 @@ module.exports = {
 	description: 'Just bought stuff!',
   args: 1,
   usage: '<stuff>',
-	execute(message, args) {
+	execute(message, args, mongo) {
     let stuffBought = args[0];
     if (!buyableStuff[stuffBought]) {
       message.reply(`congrats on your new ${stuffBought} but I'm not tracking that yet!`);
     } else {
       // TODO: retrieve a real value indexed by stuffBought
-      let fakeCounter = 0;
-      message.channel.setTopic(`${fakeCounter} day(s) since ${stuffBought} ${buyableStuff[stuffBought]} been bought! Last one to buy ${stuffBought}: ${message.author.username}`)
+      updateMongoPurchases(stuffBought, message.author.username, mongo);
+      message.channel.setTopic(`0 day(s) since ${stuffBought} ${buyableStuff[stuffBought]} been bought! Last one to buy ${stuffBought}: ${message.author.username}`)
         .then(updated => message.channel.send(`SPENT IT! ${message.author.username} bought skis! The channel topic has been updated.`));
     }
 	},
