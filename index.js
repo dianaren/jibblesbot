91d11d2eae8e6ebaf5a0a9f02027a69cd4699e30
Diana Ren <dren@tenable.com>
2020-10-01 22:10:50 -0700

    test mongodb connection

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const fs = require('fs');
 const Discord = require('discord.js');
-const { prefix } = require('./config.json');
+const MongoClient = require('mongodb').MongoClient;
 
-const client = new Discord.Client();
-client.commands = new Discord.Collection();
+const { prefix, mongoConnectionString } = require('./config.json');
+mongoConnectionString = mongoConnectionString.replace('%%PASSWORD%%', env.MONGODB_PW)
+const mongo = new MongoClient(mongoConnectionString, { useNewUrlParser: true });
+
+const discord = new Discord.Client();
+discord.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
-  client.commands.set(command.name, command);
+  discord.commands.set(command.name, command);
 }
 
 function handleMessage(message) {
@@ -17,12 +21,12 @@ function handleMessage(message) {
 
   const args = message.content.slice(prefix.length).trim().split(/ +/);
   const commandName = args.shift().toLowerCase();
-  if (!client.commands.has(commandName)) {
+  if (!discord.commands.has(commandName)) {
     return message.reply('Command not found!');
     // TODO: print valid commands
   }
 
-  const command = client.commands.get(commandName);
+  const command = discord.commands.get(commandName);
 
   if (command.args && args.length != command.args) {
     let reply = `incorrect number of arguments. Expected ${command.args}; received ${args.length}`;
@@ -42,13 +46,20 @@ function handleMessage(message) {
   }
 }
 
-client.once('ready', () => {
+discord.once('ready', () => {
+  try {
+    await mongo.connect();
+    await mongo.db("default").command({ ping: 1 });
+    console.log("Connected successfully to mongo");
+  } finally {
+    await mongo.close();
+  }
   console.log('Ready!');
 });
 
-client.login(process.env.TOKEN);
+discord.login(process.env.TOKEN);
 
-client.on('message', handleMessage);
+discord.on('message', handleMessage);
 
 module.exports = {
   handleMessage: handleMessage
