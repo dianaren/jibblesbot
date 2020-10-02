bffd8e6d81439a18e0ec1f02a4e11a2789c39bea
Diana Ren <dren@tenable.com>
2020-10-01 17:27:39 -0700

    shitty first unit tests

--- tests/commands/bought.test.js
+++ tests/commands/bought.test.js
@@ -1,24 +1,28 @@
-const bought = require('../commands/bought');
+const bought = require('../../commands/bought');
 
 const mockSend = jest.fn();
 const mockSetTopic = jest.fn(() => Promise.resolve({
   then: (fn) => fn()
 }));
+const mockReply = jest.fn();
+
+const dummyMessage = {
+  channel: {
+    send: mockSend,
+    setTopic: mockSetTopic
+  },
+  author: {
+    username: ''
+  },
+  reply: mockReply
+};
 
 describe('bought skis', () => {
   const args = ['skis'];
-  const dummyMessage = {
-    channel: {
-      send: mockSend,
-      setTopic: mockSetTopic
-    },
-    author: {
-      username: ''
-    }
-  };
   beforeEach(() => {
     mockSend.mockClear();
     mockSetTopic.mockClear();
+    mockReply.mockClear();
     bought.execute(dummyMessage, args);
   });
   test('it sets the topic once', async () => {
@@ -26,5 +30,37 @@ describe('bought skis', () => {
   });
   test('it sends a message once', async () => {
     expect(dummyMessage.channel.send).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe('bought gear', () => {
+  const args = ['gear'];
+  beforeEach(() => {
+    mockSend.mockClear();
+    mockSetTopic.mockClear();
+    mockReply.mockClear();
+    bought.execute(dummyMessage, args);
+  });
+  test('it sets the topic once', async () => {
+    expect(dummyMessage.channel.setTopic).toHaveBeenCalledTimes(1);
+  });
+  test('it sends a message once', async () => {
+    expect(dummyMessage.channel.send).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe('bought dildo', () => {
+  const args = ['dildo'];
+  beforeEach(() => {
+    mockSend.mockClear();
+    mockSetTopic.mockClear();
+    mockReply.mockClear();
+    bought.execute(dummyMessage, args);
+  });
+  test('it does not set the topic', async () => {
+    expect(dummyMessage.channel.setTopic).not.toHaveBeenCalled();
+  });
+  test('it sends a reply once', async () => {
+    expect(dummyMessage.reply).toHaveBeenCalledTimes(1);
   });
 });
